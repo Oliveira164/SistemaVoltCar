f023f66120380f96fb00db9af55f31f3ad6994c3
Gustavo <gustavooliveiracosta349@gmail.com>
2025-11-17 19:54:42 -0300

    2 - Adicionando o script do banco de dados mysql

---
+++ script.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE dbSistemaVoltCar;
+
+USE dbSistemaVoltCar;
+
+CREATE TABLE Usuario (
+    IdUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Senha VARCHAR(8) NOT NULL
+);
+
+CREATE TABLE Fornecedor (
+    IdFornecedor INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(100) NOT NULL,
+    CNPJ INT(14) UNIQUE NOT NULL,
+    Telefone Decimal(11,0)
+);
+
+CREATE TABLE Veiculo (
+    IdVeiculo INT PRIMARY KEY AUTO_INCREMENT,
+    Modelo VARCHAR(50) NOT NULL,
+    Marca VARCHAR(50) NOT NULL,
+    Ano INT,
+    Valor DECIMAL(10,2),
+    IdFornecedor INT,
+    FOREIGN KEY (IdFornecedor) REFERENCES Fornecedor(IdFornecedor)
+);
+
