7900dc70fa9fe94f0c35fcb0c4100917b75167f4
Gustavo <gustavooliveiracosta349@gmail.com>
2025-11-17 20:41:02 -0300

    6 - Criando os códigos das classes FornecedorRepositorio e UsuarioRepositorio

--- SistemaVoltCar/Repositorio/FornecedorRepositorio.cs
+++ SistemaVoltCar/Repositorio/FornecedorRepositorio.cs
@@ -1,6 +1,141 @@
-﻿namespace SistemaVoltCar.Repositorio
+﻿//Importando as bibliotecas
+using MySql.Data.MySqlClient;
+using SistemaVoltCar.Models;
+using System.Data;
+
+namespace SistemaVoltCar.Repositorio
 {
-    public class FornecedorRepositorio
+    public class FornecedorRepositorio(IConfiguration configuration)
     {
+        //Criando um campo para armazenar a string de conexão
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        //Método para cadastrar fornecedor
+        public void CadastrarFornecedor(Fornecedor fornecedor)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                var cmd = conexao.CreateCommand();
+                //Cria um comando SQL para inserir um novo dado na tabela "Fornecedor"
+                cmd.CommandText = "INSERT INTO FORNECEDOR (Nome, CNPJ, Telefone) VALUES (@nome, @cnpj, @telefone)";
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = fornecedor.Nome;
+                cmd.Parameters.Add("@cnpj", MySqlDbType.Int64).Value = fornecedor.CNPJ;
+                cmd.Parameters.Add("@telefone", MySqlDbType.Decimal).Value = fornecedor.Telefone;
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        //Método para buscar fornecedor pelo CNPJ
+        public Fornecedor ObterFornecedor(Int64 CNPJ)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                MySqlCommand cmd = new("SELECT * FROM Fornecedor WHERE CNPJ = @cnpj", conexao);
+                cmd.Parameters.AddWithValue("@cnpj", CNPJ);
+
+                // Cria um adaptador de dados (não utilizado diretamente para ExecuteReader)
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                // Declara um leitor de dados do MySQL
+                MySqlDataReader dr;
+                // Cria um novo objeto Fornecedor para armazenar os resultados
+                Fornecedor fornecedor = new Fornecedor();
+
+                /* Executa o comando SQL e retorna um objeto MySqlDataReader para ler os resultados
+                CommandBehavior.CloseConnection garante que a conexão seja fechada quando o DataReader for fechado*/
+                dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                //Lê os resultados linha por linha
+                while (dr.Read())
+                {
+                    fornecedor.IdFornecedor = Convert.ToInt32(dr["IdFornecedor"]);
+                    fornecedor.Nome = dr["Nome"].ToString();
+                    fornecedor.CNPJ = Convert.ToInt64(dr["CNPJ"]);
+                    fornecedor.Telefone = Convert.ToDecimal(dr["Telefone"]);
+                }
+                return fornecedor;
+            }
+        }
+
+        //Método para listar todos os fornecedores
+        public IEnumerable<Fornecedor> TodosFornecedores()
+        {
+            List<Fornecedor> FornecedorList = new List<Fornecedor>();
+
+            using(var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new("SELECT * FROM Fornecedor", conexao);
+
+                // Cria um adaptador de dados para preencher um DataTable com os resultados da consulta
+                MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+                // Cria um novo DataTable
+                DataTable dt = new DataTable();
+                // metodo fill- Preenche o DataTable com os dados retornados pela consulta
+                da.Fill(dt);
+                // Fecha explicitamente a conexão com o banco de dados 
+                conexao.Close();
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    FornecedorList.Add(
+                        new Fornecedor
+                        {
+                            IdFornecedor = Convert.ToInt32(dr["IdFornecedor"]),
+                            Nome = dr["Nome"].ToString(),
+                            CNPJ = Convert.ToInt64(dr["CNPJ"]),
+                            Telefone = Convert.ToDecimal(dr["Telefone"])
+                        });
+                }
+                return FornecedorList;
+            }
+        }
+
+        //Método para editar os dados do fornecedor
+        public bool Editar(Fornecedor fornecedor)
+        {
+            try
+            {
+                using (var conexao = new MySqlConnection(_conexaoMySQL))
+                {
+                    conexao.Open();
+                    // Cria um novo comando SQL para atualizar dados na tabela 'Fornecedor' com base no código do fornecedor
+                    MySqlCommand cmd = new MySqlCommand("UPDATE FORNECEDOR SET Nome = @nome, CNPJ = @cnpj, Telefone = @telefone WHERE IdFornecedor = @idFornecedor", conexao);
+
+                    cmd.Parameters.Add("@idFornecedor", MySqlDbType.Int32).Value = fornecedor.IdFornecedor;
+                    cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = fornecedor.Nome;
+                    cmd.Parameters.Add("@cnpj", MySqlDbType.Int64).Value = fornecedor.CNPJ;
+                    cmd.Parameters.Add("@telefone", MySqlDbType.Decimal).Value = fornecedor.Telefone;
+
+                    int linhasAfetadas = cmd.ExecuteNonQuery();
+                    return linhasAfetadas > 0;
+                }
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine($"Erro ao atualizar produto: {ex.Message}");
+                return false;
+            }
+        }
+
+        //Método para excluir um fornecedor
+        public void Excluir(int Id)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+
+                MySqlCommand cmd = new MySqlCommand("DELETE FROM Fornecedor WHERE IdFornecedor=@codigo", conexao);
+
+                cmd.Parameters.AddWithValue("@codigo", Id);
+
+                // Executa o comando SQL de exclusão e retorna o número de linhas afetadas
+                int i = cmd.ExecuteNonQuery();
+
+                conexao.Close();
+            }
+        }
     }
 }
--- SistemaVoltCar/Repositorio/UsuarioRepositorio.cs
+++ SistemaVoltCar/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,74 @@
-﻿namespace SistemaVoltCar.Repositorio
+﻿// Importando as bibliotecas
+using MySql.Data.MySqlClient;
+using SistemaVoltCar.Models;
+using System.Data;
+
+namespace SistemaVoltCar.Repositorio
 {
-    public class UsuarioRepositorio
+    public class UsuarioRepositorio(IConfiguration configuration)
     {
+        //Criando um campo para armazenar a string de conexão
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
+
+        //Método para cadastrar usuário
+        public void CadastrarUsuario(Usuario usuario)
+        {
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                conexao.Open();
+                var cmd = conexao.CreateCommand();
+                //Cria um comando SQL para inserir um novo dado na tabela "Usuario"
+                cmd.CommandText = "INSERT INTO USUARIO (Nome, Email, Senha) VALUES (@nome, @email, @senha)";
+
+                cmd.Parameters.Add("@nome", MySqlDbType.VarChar).Value = usuario.Nome;
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = usuario.Email;
+                cmd.Parameters.Add("@senha", MySqlDbType.VarChar).Value = usuario.Senha;
+
+                cmd.ExecuteNonQuery();
+                conexao.Close();
+            }
+        }
+
+        //Método obter usuário pelo email
+        public Usuario ObterUsuario(string email)
+        {
+            // Cria uma nova instância da conexão MySQL dentro de um bloco 'using'.
+            using (var conexao = new MySqlConnection(_conexaoMySQL))
+            {
+                // Abre a conexão com o banco de dados MySQL.
+                conexao.Open();
+                // Cria um novo comando SQL para selecionar todos os campos da tabela 'Usuario' onde o campo 'Email' corresponde ao parâmetro fornecido.
+                MySqlCommand cmd = new("SELECT * FROM Usuario WHERE Email = @email", conexao);
+                // Adiciona um parâmetro ao comando SQL para o campo 'Email', especificando o tipo como VarChar e utilizando o valor do parâmetro 'email'.
+                cmd.Parameters.Add("@email", MySqlDbType.VarChar).Value = email;
+
+                // Executa o comando SQL SELECT e obtém um leitor de dados (MySqlDataReader). O CommandBehavior.CloseConnection garante que a conexão
+                // será fechada automaticamente quando o leitor for fechado.
+                using (MySqlDataReader dr = cmd.ExecuteReader(CommandBehavior.CloseConnection))
+                {
+                    // Inicializa uma variável 'usuario' como null. Ela será preenchida se um usuário for encontrado.
+                    Usuario usuario = null;
+                    // Lê a próxima linha do resultado da consulta. Retorna true se houver uma linha e false caso contrário.
+                    if (dr.Read())
+                    {
+                        // Cria uma nova instância do objeto 'Usuario'.
+                        usuario = new Usuario
+                        {
+                            // Lê o valor da coluna "Id" da linha atual do resultado, converte para inteiro e atribui à propriedade 'Id' do objeto 'usuario'.
+                            IdUsuario = Convert.ToInt32(dr["id"]),
+                            // Lê o valor da coluna "Nome" da linha atual do resultado, converte para string e atribui à propriedade 'Nome' do objeto 'usuario'.
+                            Nome = dr["nome"].ToString(),
+                            // Lê o valor da coluna "Email" da linha atual do resultado, converte para string e atribui à propriedade 'Email' do objeto 'usuario'.
+                            Email = dr["email"].ToString(),
+                            // Lê o valor da coluna "Senha" da linha atual do resultado, converte para string e atribui à propriedade 'Senha' do objeto 'usuario'.
+                            Senha = dr["senha"].ToString()
+                        };
+                    }
+                    /* Retorna o objeto 'usuario'. Se nenhum usuário foi encontrado com o email fornecido, a variável 'usuario'
+                     permanecerá null e será retornado.*/
+                    return usuario;
+                }
+            }
+        }
     }
 }
